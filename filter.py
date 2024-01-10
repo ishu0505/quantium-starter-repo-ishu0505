d5ba52e96707622f7beeb86d828aa2b5b86787d7
ishu0505 <iparmar0505@gmail.com>
2024-01-10 16:21:50 +0530

    change

---
+++ filter.py
@@ -0,0 +1,31 @@
+import pandas
+import glob
+
+import pandas as pd
+
+combinedDataList = []
+for file in glob.glob('data/daily_sales_data_*.csv'):
+    df = pd.read_csv(file)
+
+    df = df.drop(df[df['product'] != 'pink morsel'].index)
+
+    df['price'] = df['price'].str.replace('$', '')
+
+    df['price'] = pd.to_numeric(df.price, errors='coerce')
+
+    df['sales($)'] = df['price'] * df['quantity']
+
+    df = df.drop(['quantity', 'price'], axis=1)
+
+    combinedDataList.append(df)
+
+finalData = pd.concat(combinedDataList)
+finalData = finalData.drop('product', axis=1)
+print(finalData)
+
+finalData.to_csv('data/pink_morcel_data.csv', index=False)
+
+
+
+
+# print(df)
